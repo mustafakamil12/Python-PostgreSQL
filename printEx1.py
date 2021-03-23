cb9d71fa4634a217bcd881467f54a38f915aba49
Mustafa Al Ogaidi <mustafaalogaidi@Mustafas-MacBook-Air.local>
2021-03-22 19:49:50 -0500

    done with printEx1.py

---
+++ printEx1.py
@@ -0,0 +1,4 @@
+age = input('Enter your age: ')
+#print("You have lived for " + str(int(age) * 365 * 24 * 60 * 60) + " seconds")
+#print("You have lived for " + str(int(age) * 365 * 24 * 60 * 60) + " seconds")
+print("You have lived for {} seconds, This coressponds to {} years.".format(int(age) * 365 * 24 * 60 * 60,age))
