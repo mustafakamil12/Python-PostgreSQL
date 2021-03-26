228045c911f5721bfcb3e396cbef97229a11cf04
Mustafa Al Ogaidi <mustafaalogaidi@Mustafas-MacBook-Air.local>
2021-03-26 18:43:11 -0500

    done with movie_system

--- movie-system/movie.py
+++ movie-system/movie.py
@@ -7,3 +7,21 @@ class Movie:
     def __repr__(self):
         return "<Movie: {}>".format(self.name)
 
+    def json(self):
+        return{
+            'name': self.name,
+            'genre': self.genre,
+            'watched': self.watched
+        }
+
+    @classmethod
+    def from_json(cls, json_data):  # {'genre': '...', 'name': '....', 'watched': True}
+        #return Movie(json_data['name'], json_data['genre'], json_data['watched'])
+
+        # if we need to be more explicit about what we pass and ensure
+        #return Movie(genre=json_data['genre'], watched= json_data['watched'], name= json_data['name'])
+
+        # to pass dictionary as set of name parameters
+        return Movie(**json_data)
+
+
--- movie-system/user.py
+++ movie-system/user.py
@@ -48,6 +48,27 @@ class User:
             user.movies = movies
             return user
     """
-    
+
+    def json(self):
+        return{
+            'name': self.name,
+            'movies': [
+                movie.json() for movie in self.movies
+            ]
+        }
+
+    @classmethod
+    def from_json(cls, json_data):
+        user = User(json_data['name'])
+        movies = []
+        for movie in json_data['movies']:
+            #movies.append(Movie(movie['name'], movie['genre'], movie['watched']))  <-- before create from_json in Movie class
+            movies.append(Movie.from_json(movie))
+
+        user.movies = movies
+
+        return user
+
+
 
 
