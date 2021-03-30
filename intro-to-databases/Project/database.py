fdd3b6b39dfcf7c95307e5eb42fbfb7625fca716
Mustafa Al Ogaidi <mustafaalogaidi@Mustafas-MacBook-Air.local>
2021-03-30 11:47:02 -0500

    Trying to resolve multi connection overhead

--- intro-to-databases/Project/database.py
+++ intro-to-databases/Project/database.py
@@ -6,10 +6,27 @@ def connect():
     return psycopg2.connect(database="learning", user="mustafaalogaidi", password="Pit5cxcy", host="localhost")
 
 """
-
+"""
 connection_pool = pool.SimpleConnectionPool(1,
                                             10,
                                             database="learning",
                                             user="mustafaalogaidi",
                                             password="Pit5cxcy",
                                             host="localhost")
+"""
+
+
+class ConnectionPool:
+    def __init__(self):
+        self.connection_pool = pool.SimpleConnectionPool(1,
+                                                         1,
+                                                         database="learning",
+                                                         user="mustafaalogaidi",
+                                                         password="Pit5cxcy",
+                                                         host="localhost")
+
+    def __enter__(self):
+        return self.connection_pool.getconn()
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        pass
--- intro-to-databases/Project/user.py
+++ intro-to-databases/Project/user.py
@@ -1,7 +1,8 @@
 import psycopg2
-from database import connection_pool
-#from database import connect
+from database import ConnectionPool
 
+
+# from database import connect
 
 # self is currently bounded object
 # cls is currently bounded class
@@ -56,23 +57,25 @@ class User:
     """
 
     def save_to_db(self):
-        connection = connection_pool.getconn()
-        with connection.cursor() as cursor:
-            cursor.execute('insert into users(email,first_name,last_name) values(%s,%s,%s)',
-                           (self.email, self.first_name, self.last_name))
-        connection_pool.putconn(connection)     # This line is to return connection to the pool
+        # connection = connection_pool.getconn()
+        with ConnectionPool() as connection:
+            with connection.cursor() as cursor:
+                cursor.execute('insert into users(email,first_name,last_name) values(%s,%s,%s)',
+                               (self.email, self.first_name, self.last_name))
+        # connection_pool.putconn(connection)     This line is to return connection to the pool
 
     @classmethod
     def load_from_db_by_email(cls, email):
         # to create another object withing class
         # another_user = cls('rolf@gmail.com", "Rolf", "Smith", None)
         # where cls is exactly like using User in the app
-        connection = connection_pool.getconn()
-        with connection.cursor() as cursor:
-            cursor.execute('select * from users where users.email = %s', (email,))  # here the email is inside tuple
-            user_data = cursor.fetchone() # this function is retrieve only one
-            # return new object :)
-            return cls(email=user_data[1], first_name=user_data[2], last_name=user_data[3], id=user_data[0])
+        # connection = connection_pool.getconn()
+        with ConnectionPool() as connection:
+            with connection.cursor() as cursor:
+                cursor.execute('select * from users where users.email = %s', (email,))  # here the email is inside tuple
+                user_data = cursor.fetchone() # this function is retrieve only one
+                # return new object :)
+                return cls(email=user_data[1], first_name=user_data[2], last_name=user_data[3], id=user_data[0])
 
 
 
